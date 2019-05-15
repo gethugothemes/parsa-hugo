63baa07790afabd68f5bcd4c009f871a050a23d3
somrat <monnaf37@gmail.com>
2019-05-15 14:33:21 +0600

    initialize template

---
+++ assets/js/script.js
@@ -0,0 +1,113 @@
+(function ($) {
+  'use strict';
+
+  // Preloader js    
+  $(window).on('load', function () {
+    $('.preloader').fadeOut(700);
+  });
+
+  // headroom js
+  $('.navigation').headroom();
+
+  // Background-images
+  $('[data-background]').each(function () {
+    $(this).css({
+      'background-image': 'url(' + $(this).data('background') + ')'
+    });
+  });
+
+  $('.featured-post-slider').slick({
+    dots: false,
+    speed: 300,
+    autoplay: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+
+  // Masonry
+  $(document).ready(function () {
+    $('.masonry-container').masonry({
+      itemSelector: '.masonry-container > div',
+      columnWidth: 1
+    });
+  });
+
+
+  // instafeed
+  if (($('#instafeed').length) !== 0) {
+    var userId = $('#instafeed').attr('data-userId');
+    var accessToken = $('#instafeed').attr('data-accessToken');
+    var userFeed = new Instafeed({
+      get: 'user',
+      userId: userId,
+      resolution: 'low_resolution',
+      accessToken: accessToken,
+      template: '<div class="instagram-post"><img class="img-fluid w-100" src="{{image}}" alt="instagram-image"><ul class="list-inline text-center"><li class="list-inline-item"><a href="{{link}}" target="_blank" class="text-white"><i class="ti-heart mr-2"></i>{{likes}}</a></li><li class="list-inline-item"><a href="{{link}}" target="_blank" class="text-white"><i class="ti-comments mr-2"></i>{{comments}}</a></li></ul></div>'
+    });
+    userFeed.run();
+  }
+
+  setTimeout(function () {
+    $('.instagram-slider').slick({
+      dots: false,
+      speed: 300,
+      autoplay: true,
+      arrows: false,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      responsive: [{
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 4
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2
+          }
+        }
+      ]
+    });
+  }, 2000);
+
+  // article reading time
+  $('article').each(function () {
+
+    let _this = $(this);
+
+    _this.readingTime({
+      readingTimeTarget: _this.find('.eta'),
+      remotePath: _this.attr('data-file'),
+      remoteTarget: _this.attr('data-target')
+    });
+  });
+
+
+})(jQuery);
---
+++ exampleSite/content/contact/_index.md
@@ -0,0 +1,7 @@
+---
+title: "Contact"
+date: 2019-05-14T14:46:10+06:00
+description: "This is meta description"
+image: "images/contact.jpg"
+---
+Strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream and, as I lie close to the earth, a thousand unknown plants are noticed by me.<br><br>When I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath of that universal love which bears and sustains.
