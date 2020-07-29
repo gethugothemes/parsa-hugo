adcc76bf8b574886bc0c95368b40e83d8c94871f
somratpro <abdulmonnafsomrat@gmail.com>
2020-07-29 10:05:12 +0600

    fix instagram feed

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,11 +55,9 @@
 
   // instafeed
   if (($('#instafeed').length) !== 0) {
-    var userId = $('#instafeed').attr('data-userId');
     var accessToken = $('#instafeed').attr('data-accessToken');
     var userFeed = new Instafeed({
       get: 'user',
-      userId: userId,
       resolution: 'low_resolution',
       accessToken: accessToken,
       template: '<div class="instagram-post"><img class="img-fluid w-100" src="{{image}}" alt="instagram-image"><ul class="list-inline text-center"><li class="list-inline-item"><a href="{{link}}" target="_blank" class="text-white"><i class="ti-heart mr-2"></i>{{likes}}</a></li><li class="list-inline-item"><a href="{{link}}" target="_blank" class="text-white"><i class="ti-comments mr-2"></i>{{comments}}</a></li></ul></div>'
