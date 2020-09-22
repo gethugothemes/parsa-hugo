24a4b41d878868640ea663c6a20a7502b34a27bd
somratpro <abdulmonnafsomrat@gmail.com>
2020-09-22 09:58:45 +0600

    modified instafeed

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@
       get: 'user',
       resolution: 'low_resolution',
       accessToken: accessToken,
-      template: '<div class="instagram-post"><img class="img-fluid w-100" src="{{image}}" alt="instagram-image"><ul class="list-inline text-center"><li class="list-inline-item"><a href="{{link}}" target="_blank" class="text-white"><i class="ti-heart mr-2"></i>{{likes}}</a></li><li class="list-inline-item"><a href="{{link}}" target="_blank" class="text-white"><i class="ti-comments mr-2"></i>{{comments}}</a></li></ul></div>'
+      template: '<div class="instagram-post"><a href="{{link}}" target="_blank"><img class="img-fluid w-100" src="{{image}}" alt="instagram-image"></a></div>'
     });
     userFeed.run();
   }
